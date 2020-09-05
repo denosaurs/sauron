6c7a9221df634e04ae1098452d48ca3be5137f4a
eliassjogreen <eliassjogreen1@gmail.com>
2020-09-05 15:52:38 +0200

    feat: add check_path and change check_file

--- sauron_core/src/rule.rs
+++ sauron_core/src/rule.rs
@@ -2,6 +2,7 @@ use std::{path::PathBuf, sync::Arc};
 
 /// Represents a SauronRule
 pub trait Rule<T> {
-  fn check_file(&self, _ctx: Arc<T>, _path: &PathBuf, _root: bool) {}
+  fn check_path(&self, _ctx: Arc<T>, _path: &PathBuf, _root: bool) {}
+  fn check_file(&self, _ctx: Arc<T>, _path: &PathBuf, _data: String, _root: bool) {}
   fn check_context(&self, _ctx: Arc<T>, _path: &PathBuf) {}
 }
